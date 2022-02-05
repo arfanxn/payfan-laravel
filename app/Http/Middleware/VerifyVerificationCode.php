83feed09acf17b244d01f9cfafb2db92d45d9694
mysql73 <arfan2173@gmail.com>
2022-02-05 16:59:33 +0700

    create a service and response

---
+++ app/Http/Middleware/VerifyVerificationCode.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Responses\VerificationCodeResponse;
+use App\Services\VerificationCodeService;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Validator;
+
+class VerifyVerificationCode
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $validator = Validator::make($request->all(), [
+            "email" =>  "email", "code" => "required|numeric|digits:6"
+        ]);
+        $email = $validator->validated()["email"] ?? Auth::user()->email ?? null;
+        $code = $validator->validated()["code"] ?? null;
+
+        if ($validator->fails()) return response($validator->errors()->messages(), 422);
+
+        $hashedCode = VerificationCodeService::getHashedCode($request);
+
+        if (!$hashedCode) return VerificationCodeResponse::hashedCodeNotAvailable();
+
+        $isVerified = VerificationCodeService::verify($email, $code, $hashedCode);
+
+        if ($isVerified) {
+            $response = $next($request);
+            return $response->withCookie(cookie("hashed_code", null, 1));
+        }
+
+        return VerificationCodeResponse::fail();
+    }
+}
---
+++ app/Responses/VerificationCodeResponse.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Responses;
+
+class VerificationCodeResponse
+{
+    private const Message = "verification_code_message";
+    private const ErrorMessage = "verification_code_error_message";
+
+    public static function success()
+    {
+        return response([self::Message => "Verified"], 200)
+            ->withCookie(cookie("hashed_code", null, 1));
+    }
+
+    public static function fail()
+    {
+        return response([self::ErrorMessage => "OTP or Verification Code are incorect!"], 401);
+    }
+
+    public static function hashedCodeNotAvailable()
+    {
+        return response([self::ErrorMessage =>
+        "hashed_code not found or not available or not been sent."], 401);
+    }
+
+    public static function codeSended()
+    {
+        return response([self::Message => "Verification Code has been sent to your email."]);
+    }
+}
