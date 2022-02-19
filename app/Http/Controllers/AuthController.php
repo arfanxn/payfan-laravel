f1f06a7caab52605a76f3f4a40677bf8a88c769b
mysql73 <arfan2173@gmail.com>
2022-02-19 11:26:55 +0700

    make sure name first case is uppercase

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -68,7 +68,7 @@ class AuthController extends Controller
 
         $user = User::create(array_merge(
             $validator->validated(),
-            ['password' => bcrypt($request->password)]
+            ["name" => ucwords($request->name), 'password' => bcrypt($request->password)]
         ));
 
         $token = Auth::attempt([
