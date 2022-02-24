b6ead708f62febf8247182c328ce3220ac85aa22
mysql73 <arfan2173@gmail.com>
2022-02-24 23:56:36 +0700

    adding some  method

--- app/Services/JWTService.php
+++ app/Services/JWTService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cookie;
 
 class JWTService
@@ -12,5 +13,33 @@ class JWTService
         $jwtPayload = explode(".", $jwt)[1];
         $jwtPayloadDecoded  = json_decode(base64_decode($jwtPayload));
         return $jwtPayloadDecoded;
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public static function responseNewToken($token, $message = "")
+    {
+        return response()->json([
+            'access_token' => app()->environment('local') ?  $token : "Application Environment isn't local, the \"access_token\" only sent with Cookies (httpOnly = true), due to secure our clients and data",
+            'token_type' => 'bearer',
+            'expires_in' => Auth::factory()->getTTL() * 60,
+            "message" => $message,
+            'user' => app()->environment('local') ?  Auth::user() : "Application Environment isn't local, the \"user\" only sent when Application Environment is set to local, due to secure our clients and data"
+        ])->withCookie("jwt", $token, 60 * 24);
+    }
+
+    /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public static function responseRefreshToken($message = "")
+    {
+        return self::responseNewToken(Auth::refresh(), $message);
     }
 }
