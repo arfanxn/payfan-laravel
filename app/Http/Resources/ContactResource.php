2622529aad40422114a19bbec2a23e6e8fc2df98
mysql73 <arfan2173@gmail.com>
2022-02-27 03:04:02 +0700

    make a resource

---
+++ app/Http/Resources/ContactResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ContactResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            "owner_id" => $this->owner_id,
+            "saved_id" => $this->saved_id,
+            "status" => $this->status,
+            "total_transaction" => $this->total_transaction,
+            "last_transaction" => $this->last_transaction,
+            'added_at' => $this->added_at,
+            "user" => new UserResource($this->whenLoaded("user")),
+        ];
+    }
+}
