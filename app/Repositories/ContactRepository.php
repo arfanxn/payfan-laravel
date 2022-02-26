426b4ca90ac9dff13ea3278459a9e72bab0924d3
mysql73 <arfan2173@gmail.com>
2022-02-26 23:33:50 +0700

    create method

--- app/Repositories/ContactRepository.php
+++ app/Repositories/ContactRepository.php
@@ -6,11 +6,17 @@ use App\Models\Contact;
 
 class ContactRepository
 {
+    public static function getNotBlocked(int $owner_id)
+    {
+        return Contact::with("user")->where("owner_id", $owner_id)
+            ->where("status", "!=", Contact::STATUS_BLOCKED)->orderBy("last_transaction", 'desc');
+    }
+
     public static function getUsersFromAddedContacts(int $owner_id, array $saved_ids = [])
     {
         $contacts = Contact::with("user")->where(function ($query)  use ($saved_ids, $owner_id) {
             return $query->whereIn("saved_id", $saved_ids)->where("owner_id", $owner_id);
-        })->orderBy('added_at', "desc")->get()->toArray();
+        });
         return $contacts;
     }
 }
