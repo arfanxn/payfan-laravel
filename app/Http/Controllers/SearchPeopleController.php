efa856f62ff2388badf3c29758c6eb10982cf4c1
mysql73 <arfan2173@gmail.com>
2022-02-26 23:33:22 +0700

    make a controller for searching

--- app/Http/Controllers/SearchPeopleController.php
+++ app/Http/Controllers/SearchPeopleController.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Validator;
 
 class SearchPeopleController extends Controller
 {
-    public function searchUsersNContacts(Request $request)
+    public function searchExceptSelf(Request $request) // search people except self (self == the logged in user)
     {
         $validator  = Validator::make($request->only(["keyword"]), [
             "keyword" => "required|string|max:50"
@@ -19,17 +19,18 @@ class SearchPeopleController extends Controller
         if ($validator->fails())  return response()->json($validator->errors()->messages(), 422);
         $keyword = $validator->validated()["keyword"];
 
-        $users = User::with(["isAddedBySelf"])->where(function ($query) use ($keyword) {
+        $users = User::with(["isAddedBySelf"])->where("id", "!=", Auth::id())->where(function ($query) use ($keyword) {
             return $query->where("name", "LIKE", "%$keyword%")->orWhere("email", "LIKE", "%$keyword%");
         })->orderBy("created_at", "desc")->get();
 
         $user_ids = array_map(fn ($user) => $user["id"], $users->toArray());
-        $usersInContacts = ContactRepository::getUsersFromAddedContacts(Auth::id(), $user_ids);
+        $usersInContacts = ContactRepository::getUsersFromAddedContacts(Auth::id(), $user_ids)
+            ->orderBy('added_at', "desc")->get()->toArray();
 
         return response()->json([
             "message" => ($users || $usersInContacts) ? "Results for $keyword"
                 : "No results for $keyword",
-            "users" => $users, "contacts" => $usersInContacts,
+            "users" => $users, "contacts" => $usersInContacts
         ]);
     }
 }
