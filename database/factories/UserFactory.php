fcdc18081a3cedd94513571c706f43abd2ee8d9d
mysql73 <arfan2173@gmail.com>
2022-02-26 09:59:52 +0700

    edit faker locale

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use Faker\Factory as WithFaker;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -12,10 +13,13 @@ class UserFactory extends Factory
      *
      * @return array
      */
+
     public function definition()
     {
+        $faker_ID = WithFaker::create("id_ID");
+
         return [
-            'name' => $this->faker->name(),
+            'name' => $faker_ID->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Faker\Factory as WithFaker;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,12 +14,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $faker_ID = WithFaker::create("id_ID");
         \App\Models\User::create([
             "name" => ucwords("muhammad arfan"),
             "email" => "arf@gm.com",
             "email_verified_at" => now()->toDateTimeString(),
-            "password" => bcrypt("111222")
+            "password" => bcrypt("11112222")
         ]);
-        \App\Models\User::factory(10)->create();
+
+        $totalSeed = 2000;
+        \App\Models\User::factory($totalSeed)->create();
+        \App\Models\Notification::factory($totalSeed)->create();
+
+        for ($i = 1; $i <= $totalSeed; $i++) {
+            \App\Models\UserSetting::create([
+                "user_id" => $i,
+                "two_factor_auth" => rand(0, 1),
+                "security_question" => substr(strtolower($faker_ID->sentence()), 0, 50),
+                "security_answer" => substr(strtolower($faker_ID->sentence()), 0, 50)
+            ]);
+
+            \App\Models\Contact::create([
+                "owner_id" => rand(1, 5),
+                "saved_id"  => $i + 1,
+                "status" => \App\Models\Contact::STATUS_ADDED,
+                "total_transaction" => rand(1, 99),
+                'last_transaction' => now()->subDay(rand(1, 30))->toDateTimeString(),
+                'added_at' => now()->subDays(rand(31, 365))->toDateTimeString()
+            ]);
+        }
     }
 }
