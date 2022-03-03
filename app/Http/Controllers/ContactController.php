60671720e82bf28f1b620b2d9327950e8ce6f094
mysql73 <arfan2173@gmail.com>
2022-03-04 02:57:13 +0700

    commit

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\Contact\ContactBlockedException;
 use App\Http\Resources\ContactResource;
 use App\Models\Contact;
-use App\Models\Transaction;
 use App\Models\User;
 use App\Repositories\ContactRepository;
 use App\Repositories\TransactionRepository;
+use App\Responses\ErrorsResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
@@ -21,95 +22,79 @@ class ContactController extends Controller
         return response()->json(["contacts" => $contacts]);
     }
 
-    public function lastTransactionWithContact($savedContactID)
+    public function lastTransactionDetail(Contact $contact)
+    {
+        if (Gate::denies("has-contact", $contact)) return response("Forbidden", 403);
+
+        $walletFromUserFromContact = ($contact->load("user.wallet"))->user->wallet;
+        $lastTransaction = TransactionRepository::lastTransactionWith(Auth::user()->wallet, $walletFromUserFromContact)
+            ->first();
+
+        return response()->json(["last_transaction" => $lastTransaction]);
+    }
+
+    public function toggleFavorite(Contact $contact)
     {
         try {
-            $contact = Contact::with("user.wallet")->where("owner_id",  Auth::id())->where("saved_id", $savedContactID)->firstOrFail();
-            if (Gate::denies("view-contact", $contact)) return response("Forbidden", 403);
+            if (Gate::denies("has-contact", $contact)) return response("Forbidden", 403);
 
-            $walletFromUserFromContact = $contact->user->wallet;
-            $lastTransaction = TransactionRepository::lastTransactionWith(Auth::user()->wallet, $walletFromUserFromContact)
-                ->first();
+            $message = '';
+            $statusText = '';
+            switch ($contact->status) {
+                case Contact::STATUS_FAVORITED:
+                    $contact->status = Contact::STATUS_ADDED;
+                    $message = "UNFAVORITED";
+                    $statusText = 'Contact removed from favorite';
+                    break;
+                case Contact::STATUS_BLOCKED:
+                    throw new ContactBlockedException();
+                    break;
+                default:
+                    $contact->status = Contact::STATUS_FAVORITED;
+                    $message = Contact::STATUS_FAVORITED;
+                    $statusText = 'Contact added to favorite';
+                    break;
+            }
+            $contact->save();
 
-            return response()->json(["last_transaction" => $lastTransaction]);
-        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
-            return response()->json(['error_message' => "Contact not found."], 404);
+            return response()->json(["message" => $message])->setStatusCode(200, $statusText);
+        } catch (ContactBlockedException $e) {
+            return $e;
         }
     }
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function AddOrRemove(User $user)
     {
-        //
+        $contactQuery = Contact::query()->where("owner_id", Auth::id())->where("saved_id", $user->id);
+        $isAlreadyAdded = $contactQuery->exists();
+
+        $statusText = "";
+        $message = '';
+        if ($isAlreadyAdded) {
+            $contactQuery->delete();
+            $message = "Removed";
+            $statusText = "User deleted from contact.";
+        } else {
+            Contact::query()->create([
+                "owner_id" => Auth::id(),
+                "saved_id" => $user->id,
+                "status" => Contact::STATUS_ADDED,
+                "added_at" => now()->toDateTimeString(),
+            ]);
+            $message = "Added";
+            $statusText = "User added to contact.";
+        }
+
+        return response()->json(["message" => $message])->setStatusCode(200, $statusText);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function block(Contact $contact)
     {
-        //
-    }
+        if (Gate::denies("has-contact", $contact)) return response("Forbidden", 403);
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        $contact->status = Contact::STATUS_BLOCKED;
+        return $contact->save() ?
+            response()->json(['message' => "Contact blocked successfully."])->setStatusCode(200, "Contact blocked successfully.")
+            : ErrorsResponse::server();
     }
 }
