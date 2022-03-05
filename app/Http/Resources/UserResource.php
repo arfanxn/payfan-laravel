5738554c4cdb55c981c29f60410cd9510282f2e0
mysql73 <arfan2173@gmail.com>
2022-03-05 07:10:22 +0700

    adding wallet attribute

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -24,7 +24,8 @@ class UserResource extends JsonResource
             "profile_pict" => URLHelper::userProfilePict(isset($this->profile_pict) && $this->profile_pict  ? $this->profile_pict : null),
             "created_at" => $this->created_at,
             "updated_at" => $this->updated_at,
-            "settings" => $this->whenLoaded("settings", fn () => $this->settings)
+            "settings" => $this->whenLoaded("settings", fn () => $this->settings),
+            "wallet" => $this->whenLoaded("wallet", fn () => $this->wallet),
         ];
     }
 }
