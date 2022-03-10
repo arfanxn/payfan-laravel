462021bf5b6b916d965a4f3da09ad434b6d2511b
mysql73 <arfan2173@gmail.com>
2022-03-10 11:23:45 +0700

    recreateinng migration

--- database/migrations/2022_03_10_034024_create_notifications_table.php
+++ database/migrations/2022_03_10_034024_create_notifications_table.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\Notification;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,13 +14,11 @@ class CreateNotificationsTable extends Migration
     public function up()
     {
         Schema::create('notifications', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId("user_id")->constrained("users", "id")->onDelete("cascade")->onUpdate("cascade");
-            $table->string("type", 50);
-            $table->string("title", 100);
-            $table->string("redirect")->nullable();
-            $table->string("body", 9999)->nullable();
-            $table->string("status")->default(Notification::STATUS_UNREAD);
+            $table->uuid('id')->primary();
+            $table->string('type');
+            $table->morphs('notifiable');
+            $table->text('data');
+            $table->timestamp('read_at')->nullable();
             $table->timestamps();
         });
     }
