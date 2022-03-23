2a310045085f463905d8eaeb4c79cbfb19a41d21
mysql73 <arfan2173@gmail.com>
2022-03-23 18:51:38 +0700

    edit trnsaxtion seeeder

--- database/seeders/TransactionSeeder.php
+++ database/seeders/TransactionSeeder.php
@@ -23,6 +23,10 @@ class TransactionSeeder extends Seeder
         $now = Carbon::now();
 
         Transaction::factory()->count($total)->create()->each(function (Transaction $transaction)  use ($total, $now, $faker_ID) {
+            $ordertypes = rand(0, 1) ?
+                [Order::TYPE_SENDING_MONEY, Order::TYPE_RECEIVING_MONEY] :
+                [Order::TYPE_REQUESTING_MONEY, Order::TYPE_REQUESTED_MONEY];
+
             $order1 = [
                 "id" => strtoupper(StrHelper::random(14))  . $now->timestamp,
                 "user_id" => $transaction->from_wallet,
@@ -30,7 +34,7 @@ class TransactionSeeder extends Seeder
                 "to_wallet" => $transaction->to_wallet,
                 "transaction_id" => $transaction->id,
                 "note" => $faker_ID->sentences(1, 2, true),
-                "type" => Order::TYPE_SENDING_MONEY,
+                "type" => $ordertypes[0],
                 "status" => Order::STATUS_COMPLETED,
                 "amount" => $transaction->amount,
                 "charge" => $transaction->charge,
@@ -43,7 +47,7 @@ class TransactionSeeder extends Seeder
                 "user_id" => $transaction->to_wallet,
                 "from_wallet" => $transaction->from_wallet,
                 "to_wallet" => $transaction->to_wallet,
-                "type" => Order::TYPE_RECEIVING_MONEY,
+                "type" => $ordertypes[1],
                 "charge" => null,
             ]);
             Order::insert([$order1, $order2]);
