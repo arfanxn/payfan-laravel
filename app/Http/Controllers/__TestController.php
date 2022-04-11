1322a0abc495b38e4bec5c4ca6fedcc5ff2d08d0
mysql73 <arfan2173@gmail.com>
2022-04-11 10:56:58 +0700

    testing  controller

--- app/Http/Controllers/__TestController.php
+++ app/Http/Controllers/__TestController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
+use App\Models\User;
+use App\Notifications\Transactions\SendMoneyNotification;
+use App\Repositories\ContactRepository;
 use App\Repositories\NotificationRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
@@ -17,19 +21,26 @@ class __TestController extends Controller
     public function index(Request $request)
     {
         // return \App\Models\User::limit(1)->first();
-        $user = \App\Models\User::offset(1)->limit(1)->first();
-        $orderSendMoney = \App\Models\Order::query()->where(
-            fn ($q) =>
-            $q->where("type", \App\Models\Order::TYPE_SENDING_MONEY)
-                ->where("status", "COMPLETED")
-        )->orderBy("started_at", "desc")->first();
+        // $user = \App\Models\User::offset(1)->limit(1)->first();
+        // $orderSendMoney = \App\Models\Order::query()->where(
+        //     fn ($q) =>
+        //     $q->where("type", \App\Models\Order::TYPE_SENDING_MONEY)
+        //         ->where("status", "COMPLETED")
+        // )->orderBy("started_at", "desc")->first();
 
-        $isSent = \Illuminate\Support\Facades\Notification::send(
-            $user,
-            new \App\Notifications\Transactions\SendMoneyNotification($orderSendMoney)
-        );
+        // $isSent = \Illuminate\Support\Facades\Notification::send(
+        //     $user,
+        //     new \App\Notifications\Transactions\SendMoneyNotification($orderSendMoney)
+        // );
 
-        return dd($isSent);
+        // return dd($isSent);
+        // ContactRepository::incrementAndUpdate_LastTransactionAndTotalTransaction_whereOwnerIdOrSavedId([4, 5]);
+
+        $notification1 = \App\Models\User::skip(1)->first()->notifications()->first();
+        $notification2 = (array) NotificationRepository::make()->where_Notifiable(2)->getBuilder()->first();
+        $notification2['data'] = json_decode($notification2['data']);
+
+        return response()->json([$notification1, $notification2]);
     }
 
     public function previewMailNotification(Request $request)
@@ -48,18 +59,23 @@ class __TestController extends Controller
         return $notification->render();
     }
 
-    public function triggerEvent()
+    public function triggerNotificationBroadcast()
     {
-        $event = event(new \App\Events\NewNotificationEvent(
-            NotificationRepository::make()->getBuilder()
-                ->where("notifiable_id", 2)->orderBy('created_at', 'DESC')->first(),
-        )/**/);
+        // $event = event(new \App\Events\NewNotificationEvent(
+        //     NotificationRepository::make()->getBuilder()
+        //         ->where("notifiable_id", 2)->orderBy('created_at', 'DESC')->first(),
+        //     \App\Models\User::skip(1)->first(),
+        // )/**/);
+
+        $user = User::query()->where("id", 2)->first();
+        $order = Order::query()
+            ->where("user_id", $user->id)
+            ->where("type", Order::TYPE_SENDING_MONEY)->inRandomOrder(1)->first();
+
+        $user->notify(new SendMoneyNotification($order));
 
         return response()->json(
-            [
-                $event,
-                "message" => "Event has been sent!"
-            ],
+            ["message" => "Notification has been sent!", "order" => $order],
         );
     }
 
